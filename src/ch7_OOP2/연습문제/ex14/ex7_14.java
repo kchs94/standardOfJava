478a50656c444e34dce4727c7ce69388097b10c5
kchs94 <kchs94@gmail.com>
2022-05-13 19:34:21 +0900

    Add ex7_14.java

--- src/ch7_OOP2/연습문제/ex14/ex7_14.java
+++ src/ch7_OOP2/연습문제/ex14/ex7_14.java
@@ -1,7 +1,30 @@
 package ch7_OOP2.연습문제.ex14;
-
+/*
+* - 원래 변수 앞에 fianl을 붙일 때는 선언과 동시에 초기화를 해줘야 한다.
+* 그러나, 인스턴스 변수의 경우 생성자를 통해서도 초기화를 할 수 있다.
+* */
 public class ex7_14 {
     public static void main(String[] args) {
 
+        SutdaCard card = new SutdaCard(1, true);
+    }
+}
+
+class SutdaCard {
+    final int NUM;
+    final boolean IS_KWANG;
+
+    SutdaCard() {
+        this(1, true);
+    }
+
+    // 생성자 안에서 상수를 초기화 한다.
+    SutdaCard(int NUM, boolean IS_KWANG) {
+        this.NUM = NUM;
+        this.IS_KWANG = IS_KWANG;
+    }
+
+    public String toString() {
+        return NUM + (IS_KWANG ? "K" : "");
     }
 }
